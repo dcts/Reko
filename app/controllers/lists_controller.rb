e49608bcdbd82b0f9e28ee1df6022550b064e153
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-05-25 20:39:32 +0200

    lists#index and lists#show

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,11 @@
 class ListsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   # define landing page (at the moment the backend control panel)
   root to: 'pages#home'
   # list routes
-  resources :lists, only: [:index]
+  resources :lists, only: [:index, :show]
 end
