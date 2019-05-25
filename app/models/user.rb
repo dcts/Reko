88fe4abcdbf071c8b6ac772bbd5f56b4c2dc52a8
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-05-26 00:32:28 +0200

    change list to rekos

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :lists
+  has_many :rekos
 
   def to_s
     "#{first_name} #{last_name}"
