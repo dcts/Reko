1ab82df17b46b3df7ed9f9671b165e7a09073da5
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-05-26 14:40:46 +0200

    modified db-schema (add year to movie)

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_05_25_124657) do
     t.integer "itunes_id"
     t.string "image_url"
     t.string "title"
+    t.integer "year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
