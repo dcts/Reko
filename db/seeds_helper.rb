7c7e94f37929e060fcecdbf4fb0cb9f3a6e257b1
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-05-28 00:09:41 +0200

    add seed for preferences

--- db/seeds_helper.rb
+++ db/seeds_helper.rb
@@ -9,6 +9,8 @@ end
 def reset_database
   puts "deleting all database entries (rekos, movies, users"
   # ordering of the delete operations matters!
+  UserPreference.delete_all
+  Preference.delete_all
   Reko.delete_all
   Movie.delete_all
   User.delete_all
@@ -17,6 +19,33 @@ end
 def create_user(attr)
   puts "creating user #{attr[:first_name]}"
   User.create!(attr)
+end
+
+def create_preferences(preference_names)
+  preference_names.each do |name|
+    Preference.create!(name: name)
+    puts "creating preference category #{name}"
+  end
+end
+
+# CREATE USER PREFERENCES
+# randomly assignment of user preferences
+# 33%: Movies
+# 33%: Documentaris
+# 33%: Movies + Documentaries
+def create_user_preferences
+  movie_pref = Preference.where(name: "Movies").first # get preference instance with name "Movies"
+  doc_pref = Preference.where(name: "Documentaries").first # get preference instance with name "Documentaries"
+  User.all.each do |user|
+    puts "creating user preferences for user #{user.to_s}"
+    random = rand
+    add_preferences(user, [movie_pref]) if random <= 0.33
+    add_preferences(user, [doc_pref]) if random > 0.33 && random <= 0.66
+    add_preferences(user, [movie_pref, doc_pref]) if random > 0.66
+  end
+end
+def add_preferences(user, pref_arr)
+  pref_arr.each { |pref| UserPreference.create!(user: user, preference: pref)}
 end
 
 # CREATE REKO METHOD
