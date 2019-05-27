662e597497e917d0dc5821d821c43ffdfb115bad
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-05-27 21:06:04 +0200

    update admin routes and admins controller routes

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,2 +1,24 @@
 class AdminsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:users] #, :rekos, :recommendables, :preferences, :beta_applicants]
+  # if you are an admin, you can see all tables
+
+  def users
+    @users = User.all
+  end
+
+  # def rekos
+
+  # end
+
+  # def recommendables
+
+  # end
+
+  # def preferences
+  #   @prefernces = Preference.all
+  # end
+
+  # def beta_applicants
+
+  # end
 end
