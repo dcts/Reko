a90731efd9378583b0649e31d21721e52e384e37
Assunta Walderdorff <assunta@walderdorff.org>
2019-05-28 18:54:20 +0200

    back to simple form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    current_user_path(current_user.id)
+    rekos_path
   end
 end
