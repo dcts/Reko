1af9568eb7fe8da96547203ec79bfce3b241f349
Assunta Walderdorff <assunta@walderdorff.org>
2019-05-28 17:58:46 +0200

    seed works thanks andy

--- app/models/reko.rb
+++ app/models/reko.rb
@@ -7,7 +7,7 @@ class Reko < ApplicationRecord
   # eventually we will want to do this:
   # belongs_to :sender, class_name: "User", foreign_key: "sender_id"
   belongs_to :recommendable, polymorphic: true
-  belongs_to :movie, -> { where(rekos: {recommendable_type: 'Movie'}) }, foreign_key: 'recommendable_id'
+  belongs_to :movie, -> { where(rekos: {recommendable_type: 'Movie'}) }, foreign_key: 'recommendable_id', optional: true
 
 
   validates :sender_name, length: { minimum: 2 }
