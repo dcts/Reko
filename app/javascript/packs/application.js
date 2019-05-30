21deffc3d68f49e4c5023ce2f1b881cc2b6f2d86
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-05-30 14:59:04 +0200

    working admins/test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,8 +3,11 @@ import { copyLink } from "../components/copylink";
 import { makeSwipe } from "../components/swipe";
 import { searchFlow } from "../api-search/add-rekos-flow";
 import toggleButtons from "../components/user-preference-choice";
+import { copyToClipboard, test } from "../components/copylink-new";
+
 
 copyLink();
 makeSwipe();
 toggleButtons();
 searchFlow();
+test();
