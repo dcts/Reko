1022c19a7e038d0e0cd5a1bad82ba198e30f4ea0
Assunta Walderdorff <assunta@walderdorff.org>
2019-06-01 17:57:01 +0200

    modal working

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -2,6 +2,11 @@ class FeedbacksController < ApplicationController
   skip_before_action :authenticate_user!, only: [:new, :create]
 
   def create
-    @feedback = Feedback.new(params[:content])
+    @feedback = Feedback.new(feedback_params)
+    @feedback.save!
+  end
+
+  def feedback_params
+    params.require(:feedback).permit(:content)
   end
 end
