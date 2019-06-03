16545616758221a901d19239a5164a37f1a497ac
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-06-03 20:11:29 +0200

    adapt visitor token in controller

--- app/controllers/rekos_controller.rb
+++ app/controllers/rekos_controller.rb
@@ -4,7 +4,7 @@ class RekosController < ApplicationController
   def index
     @user_movies = Reko.left_outer_joins(:movie).where(receiver_id: current_user.id)
     @movies = sort_rekos(@user_movies.open, @user_movies.done)
-    @visitor_link = request.original_url.gsub("/rekos", "/rekos/new?token=#{current_user.token}")
+    @visitor_link = request.original_url.gsub("/rekos", "/rekos/new/onboarding?token=#{current_user.token}")
   end
 
   def onboarding
