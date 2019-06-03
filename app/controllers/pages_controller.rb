2e74e20672db1c0daecd8d6b87ed3d247ac03b7a
Assunta Walderdorff <assunta@walderdorff.org>
2019-06-03 09:02:15 +0200

    start styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,9 @@ class PagesController < ApplicationController
     @token = params[:token]
     @receiver = User.find_by_token(@token)
     @sender_name = params[:name]
-    @movies = params[:movies].split("_and_")
+    if params[:movies]
+      @movies = params[:movies].split("_and_")
+    end
     # redirect if token is invalid
     redirect_to(root_path) if @receiver.nil?
   end
