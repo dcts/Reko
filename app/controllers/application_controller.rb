4099b6fff8c407826b33965dfbae95ea7c6ce630
Thomas Starzynski <thomas.starzynski@unibas.ch>
2019-11-21 12:11:21 +0100

    fix issue ##207

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
@@ -20,6 +19,6 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-  { host: ENV["DOMAIN"] || "localhost:3000" }
+    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 end
--- app/controllers/user_preferences_controller.rb
+++ app/controllers/user_preferences_controller.rb
@@ -12,21 +12,16 @@ class UserPreferencesController < ApplicationController
   def create
     #This returns an array of integers with selected preference ids!
     preference_ids=params[:user_preferences].map(&:to_i)
-
     if params[:user_preferences]
       pref = UserPreference.where(user_id: current_user.id)
       pref.each { |p| p.destroy! }
-
       # @user_preference =  UserPreference.new(user_preference_params)
       @preferences = Preference.where("id IN (#{preference_ids.join(',')})")
       @user = current_user
-
       @preferences.each do |pref|
         user_preference = UserPreference.create(user:@user, preference:pref)
       end
-
       redirect_to rekos_path
-
     else
       render :new
     end
